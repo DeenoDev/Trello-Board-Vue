22354b524bb90956e6e7f6805dd0bffab1753a60
DeenoDev <deyanoklaasen@outlook.com>
2024-03-24 00:08:39 +0200

    Started Nuxt app by using CLI and turned off server side rendering.

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "a5fdb8c0-bc32-4ae2-b9a1-7d33bd73aa5f"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /*@__PURE__*/ defuFn(inlineConfig)
