9c9f346746893981579a74c259788e7e0b722ccf
DeenoDev <deyanoklaasen@outlook.com>
2024-03-25 00:31:40 +0200

    Added TrelloBoard heading and my github profile pic to UI.

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="p-10 h-[100vh] bg-teal-700 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">
+      <img 
+      width="200"
+      class="mr-3 border-rounded"
+      src="https://github.com/DeenoDev.png" 
+      alt="DeenoDev">
+
+      Trello Board
+       
+    </h1>
     <TrelloBoard />
   </div>
 </template>
--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="flex gap-4 overflow-x-auto items-start">
         <div v-for="column in columns" key="column.id" class="column bg-gray-200 p-5 rounded min-w-[250px]">
-            <header>
+            <header class="font-bold mb-4">
                 {{ column.title }}
             </header>
             <TrelloBoardTask v-for="task in column.tasks" :task="task"/> 
+            <footer>
+                <button class="text-gray-500">
+                    Add a Card
+                </button>
+            </footer>
         </div>
     </div>
 
