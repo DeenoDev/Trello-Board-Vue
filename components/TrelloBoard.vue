07af9a03aefccc9d76ed5b00c55418aa527c6152
DeenoDev <deyanoklaasen@outlook.com>
2024-03-24 22:26:15 +0200

    Hard coded the rest of the arrays of objects into TrelloBoard.vue file.

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -8,8 +8,29 @@ import type { Column } from '~~/types';
 const columns = ref<Column[]>([
     {
         id: nanoid(),
-        title: "Create marketing landing page",
-        tasks: [],
+        title: "Backlog",
+        tasks: [
+        {
+            title: "Create marketing landing page", 
+            createdAt: new Date(),
+            id: nanoid(), 
+        },
+        {
+            title: "Create marketing landing page", 
+            createdAt: new Date(),
+            id: nanoid(), 
+        },
+        {
+            title: "Fix page nav bug", 
+            createdAt: new Date(),
+            id: nanoid(), 
+        },
+    ],
     },
+        {title: "Selected for Dev", id: nanoid(), tasks:[]},
+        {title: "In Progress", id: nanoid(), tasks:[]},
+        {title: "QA", id: nanoid(), tasks:[]},
+        {title: "Complete", id: nanoid(), tasks:[]},
+    
 ]);
 </script>
