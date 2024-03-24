251547ac7b615a398549d1f0f4c7a2ff11e96625
DeenoDev <deyanoklaasen@outlook.com>
2024-03-25 01:17:11 +0200

    DragHandle.vue component created for draggable functionality.

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -3,12 +3,15 @@
         <draggable 
         v-model="columns"
          group="columns"
+         :animation="150"
+         handle=".drag-handle"
          item-key="id"
          class="flex gap-4 overflow-x-auto items-start"
          >
         <template #item="{element: column}: {element: Column}">
             <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
               <header class="font-bold mb-4">
+                <span class="drag-handle cursor-move"> 🖐️ </span>
                  {{ column.title }}
               </header>
               <TrelloBoardTask v-for="task in column.tasks" 
