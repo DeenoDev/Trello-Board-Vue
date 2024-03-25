d7092421225fd28b7446a4b56ea299c36c687890
DeenoDev <deyanoklaasen@outlook.com>
2024-03-25 02:55:44 +0200

    NewTask script completed.

---
+++ components/NewTask.vue
@@ -0,0 +1,30 @@
+<template>
+
+
+</template>
+
+<script setup lang="ts">
+import type {Task} from '~~/types';
+import { nanoid } from 'nanoid';
+
+const emit = defineEmits<{
+    (e: "add", payload: Task): void;
+}>();
+
+const focused = ref(false);
+const title = ref("");
+
+function createTask(e: Event){
+    if (title.value.trim()){
+        e.preventDefault();
+        emit("add", {
+            title: title.value.trim(),
+            createdAt: new Date(),
+            id: nanoid(),
+        } as Task)
+    }
+
+    title.value = "";
+}
+
+</script>
