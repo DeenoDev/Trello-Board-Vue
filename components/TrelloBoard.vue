3f9f696b2c2ab7f5f8a5452da31c7c8eeb94d54b
DeenoDev <deyanoklaasen@outlook.com>
2024-03-25 04:18:34 +0200

    fixed title input bug on new cards.

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -12,7 +12,7 @@
             <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
               <header class="font-bold mb-4">
                 <DragHandle />
-                 <input class="bg-transparent focus:bg-white rounded px-1 w-4/5" 
+                 <input class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5" 
                  @keyup.enter="($event.target as HTMLInputElement).blur()" 
                  type="text"
                  v-model="column.title">
@@ -90,7 +90,11 @@ function createColumn(){
         tasks: [],
     }
     columns.value.push(column);
-}
+    nextTick(()=>{
+        (document.querySelector(".column:last-of-type .title-input")as HTMLInputElement).focus();
+    });
+    
+};
 
 </script>
 
